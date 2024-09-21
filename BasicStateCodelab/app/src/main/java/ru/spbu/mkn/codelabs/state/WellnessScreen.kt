1c85388073c5e9e8b962e5354c3008ceb6d7bae5
arnyyyyy <garrybobcrabsburger@gmail.com>
2024-09-21 19:34:59 +0300

    3.7 state hoisting

--- BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessScreen.kt
@@ -1,7 +1,8 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import ru.spbu.mkn.codelabs.state.StatefulCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
--- BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessTaskItem.kt
+++ BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessTaskItem.kt
@@ -1,30 +1,21 @@
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
+package ru.spbu.mkn.codelabs.state
 
-@Composable
-fun WellnessTaskItem(
-    onClose = { showTask = false },
-    taskName = "Have you taken your 15 minute walk today?",
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
-            text = taskName
-        )
-        IconButton(onClick = onClose) {
-            Icon(Icons.Filled.Close, contentDescription = "Close")
-        }
-    }
-}
+
+//@Composable
+//fun WellnessTaskItem(
+//    onClose = { showTask = false },
+//    taskName = "Have you taken your 15 minute walk today?",
+//    modifier: Modifier = Modifier
+//) {
+//    Row(
+//        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+//    ) {
+//        Text(
+//            modifier = Modifier.weight(1f).padding(start = 16.dp),
+//            text = taskName
+//        )
+//        IconButton(onClick = onClose) {
+//            Icon(Icons.Filled.Close, contentDescription = "Close")
+//        }
+//    }
+//}
