aae59c493f9e1b57a52be6721715e311eb7137e8
arnyyyyy <garrybobcrabsburger@gmail.com>
2024-09-21 23:46:49 +0300

    3.9 observable mutable list

--- BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessScreen.kt
@@ -1,14 +1,23 @@
+package ru.spbu.mkn.codelabs.state
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import ru.spbu.mkn.codelabs.state.StatefulCounter
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
+import ru.spbu.mkn.codelabs.state.WellnessTask
+import ru.spbu.mkn.codelabs.state.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
+
 
 private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
--- BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/ru/spbu/mkn/codelabs/state/WellnessTask.kt
@@ -1,1 +1,3 @@
+package ru.spbu.mkn.codelabs.state
+
 data class WellnessTask(val id: Int, val label: String)
